9d45fd748868a912f5965d659c21b3b3674fe7c9
LeeJungGeun <59906411+LeeJungGeun@users.noreply.github.com>
2020-02-21 14:34:38 +0900

    괄호를 열지않고 닫기만했을때 에러가 나오지 않던점 수정

--- Calculator/Calculator_base.h
+++ Calculator/Calculator_base.h
@@ -26,7 +26,7 @@ public:
 	int GetLength() { return _length == 0 ? _question.length() : _length; } // 연산한 항목의 길이를 반환.(재귀호출 체크용) 아직 연산을 수행하지 않았다면 질의 전체의 길이를 반환
 	void Initialize(); // 내용을 초기화
 	int Calculate(); // 연산 함수. 성공시 0을, 실패시 -1을 리턴
-
+	
 protected:
 	wstring _question; // 연산 문장을 넣는 변수
 	double _answer; // 결과값을 넣는 변수
@@ -35,10 +35,11 @@ protected:
 
 	int _errorCode; // 연산 결과 성공여부 - 0:성공, 1:실패, 기타 숫자 : 에러 종류.(div/0 등)
 	int _length; // 연산한 문장 길이 체크용
-
+	int in; //괄호 내부인지 아닌지 체크
 private:
 	int calculate_Sub(map<int, wstring>& numbers, map<int, int>& operators); // 3개의 항을 받아 연산하는 함수. 성공시 0을, 실패시 -1을 리턴
 	int calculate_Simple(double input1, double input2, int operation, double& result); // 단순 두 항을 연산하는 함수. 성공시 0을, 실패시 -1을 리턴
 	void setError(int errorCode) { _errorCode = errorCode; };
 	int parseQuestion(); // 문장을 값과 연산자로 구분
+	
 };
